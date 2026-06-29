624173d5421e242240430c96a1a7fde94550e9a6
Musrat001 <mohdmusrat73@gmail.com>
2026-06-29 19:47:48 +0530

    added the feature of password update based on email

---
+++ updatePassword.js
@@ -0,0 +1,61 @@
+const showPassword = document.getElementById("showPassword");
+
+const password = document.getElementById("password");
+
+const confirmPassword = document.getElementById("confirmPassword");
+
+showPassword.addEventListener("change", () => {
+
+    const type = showPassword.checked ? "text" : "password";
+
+    password.type = type;
+    confirmPassword.type = type;
+
+});
+
+const updatePasswordForm = document.getElementById("updatePasswordForm");
+
+
+updatePasswordForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    try {
+
+        const passwordUpdateData = {
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            confirmPassword: document.getElementById("confirmPassword").value,
+        }
+
+        const response = await fetch(
+            "https://protfolio-backend-45v7.onrender.com/v1/setPassword",
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+
+                },
+                body: JSON.stringify(passwordUpdateData)
+            }
+        );
+
+        const data = await response.json();
+        if (!response.ok) {
+            alert(data.message);
+            return;
+        }
+        if (data) {
+            console.log(data);
+            // registerCard.style.display = "none";
+            // successMessage.innerText = data.message;
+            alert(data.message);
+            setTimeout(() => {
+                window.location.href = "index.html";
+            }, 1000);
+
+        }
+
+    } catch (error) {
+        console.log("Error while updating password", error);
+
+    }
+})
